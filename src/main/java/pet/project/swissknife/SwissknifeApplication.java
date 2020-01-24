47d3a7f5c6c7d094d4758b5bbac5e211b6cdc36c
EvgeniyVishnevskiy <w1sh.i.am.prestige@gmail.com>
2020-01-24 16:08:00 +0300

    Initial commit

---
+++ src/main/java/pet/project/swissknife/SwissknifeApplication.java
@@ -0,0 +1,13 @@
+package pet.project.swissknife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwissknifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwissknifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pet/project/swissknife/SwissknifeApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.project.swissknife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwissknifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
