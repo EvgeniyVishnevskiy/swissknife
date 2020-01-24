42ee5af7aec10f8eeef5abd85f91b7b68e73c579
EvgeniyVishnevskiy <w1sh.i.am.prestige@gmail.com>
2020-01-24 16:27:04 +0300

    Rename main class

--- src/main/java/pet/project/swissknife/ApplicationRun.java
+++ src/main/java/pet/project/swissknife/ApplicationRun.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SwissknifeApplication {
+public class ApplicationRun {
 
     public static void main(String[] args) {
-        SpringApplication.run(SwissknifeApplication.class, args);
+        SpringApplication.run(ApplicationRun.class, args);
     }
 
 }
